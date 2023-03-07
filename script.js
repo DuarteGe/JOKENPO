1baaf1b68ca097fe3f00d3fce9a8dad88a38f2cc
DuarteGe <111998883+DuarteGe@users.noreply.github.com>
2023-03-07 19:16:55 -0300

    Tentando outras formas de chamar a função

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
 window.getElementById("pedra").addEventListener("click", escolha(1))
-document.getElementById("papel").addEventListener("click", escolha(2))
-document.getElementById("tesoura").addEventListener("click", escolha(3))
+/*document.getElementById("papel").addEventListener("onclick", escolha())
+document.getElementById("tesoura").addEventListener("click", escolha(3))*/
 
 
 var resulta = document.getElementById("result")
 var possibilidades = ['Pedra', 'Papel', 'Tesoura']
 
-function escolha(retorno){
-    switch (retorno){
-        case 1:
-            window.alert('pedra')
-        break;
-        case 2:
-            window.alert('papel')
-        break;
-    }
-    //window.alert(pedra.value)
+function escolha(){
+window.alert(pedra.value)
 }
